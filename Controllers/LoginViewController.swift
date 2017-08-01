521d7c0082939027a2284d5182949509bc913cb6
LanceMargarett <lalancie@gmail.com>
2017-08-01 13:53:21 -0400

    This commit imported the Firebase Databse reader file

--- Controllers/LoginViewController.swift
+++ Controllers/LoginViewController.swift
@@ -6,4 +6,64 @@
 //  Copyright © 2017 PhilyLa. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
+
+
+typealias FIRUser = FirebaseAuth.User
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        // 1
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        
+        // 2
+        authUI.delegate = self
+        
+        // 3
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+    }
+    
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        // 1
+        guard let user = user
+            else { return }
+        
+        // 2
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        // 3
+      userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let user = User(snapshot: snapshot) {
+                print("Welcome back, \(user.username).")
+            } else {
+                print("New user!")
+            }
+        })
+    }
+}
---
+++ User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Makestagram
+//
+//  Created by Lance Margarett Panes on 8/1/17.
+//  Copyright © 2017 PhilyLa. All rights reserved.
+//
+
+import Foundation
