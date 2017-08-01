585c30d1b53d67b8a9e89cd59635eb410a8d5e80
LanceMargarett <lalancie@gmail.com>
2017-08-01 19:29:08 -0400

    This commit finished step 15

---
+++ Helper/MGPhotoHelper.swift
@@ -0,0 +1,13 @@
+//
+//  MGPhotoHelper.swift
+//  Makestagram
+//
+//  Created by Lance Margarett Panes on 8/1/17.
+//  Copyright © 2017 PhilyLa. All rights reserved.
+//
+
+import UIKit
+
+class MGPhotoHelper: NSObject {
+
+}
